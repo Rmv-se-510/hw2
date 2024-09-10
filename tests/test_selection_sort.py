d246dee4325766bb1e03134fc5c7eb6e7b949e6b
Meet Vora <meetvora090201@gmail.com>
2024-09-11 02:12:33 +0530

    selection sort test case and successful test execution snapshot added

--- tests/test_selection_sort.py
+++ tests/test_selection_sort.py
@@ -1,6 +1,13 @@
 import warnings
 import pytest
-from hw2_debugging import selectionSort
+import sys
+import os
+
+sys.path.append(os.path.join(os.getcwd()))
+sys.path.append(os.path.join(os.getcwd(), "sorting_algorithms"))
+
+from sorting_algorithms.selectionSort import selection_sort
+
 
 warnings.filterwarnings('ignore')
 
@@ -8,17 +15,15 @@ def test_large_array():
     """
     Test with a large array which contains 10 elements
     """
-  
     large_array = [100, 24, 56, 12, 98, 3, 67, 45, 29, 88]
-    assert selectionSort(large_array) == sorted(large_array)
+    assert selection_sort(large_array) == sorted(large_array)
 
 def test_array_with_duplicates():
     """
     Test with aa array which contains duplicate elements
     """
-  
     array_with_duplicates = [4, 2, 2, 8, 4, 6, 1, 9, 1]
-    assert selectionSort(array_with_duplicates) == sorted(array_with_duplicates)
+    assert selection_sort(array_with_duplicates) == sorted(array_with_duplicates)
 
 def test_negative_numbers():
     """
@@ -26,7 +31,7 @@ def test_negative_numbers():
     """
   
     array_with_negatives = [-5, -1, -10, 3, 0, 7, -3]
-    assert selectionSort(array_with_negatives) == sorted(array_with_negatives)
+    assert selection_sort(array_with_negatives) == sorted(array_with_negatives)
 
 def test_mixed_positive_negative():
     """
@@ -34,7 +39,7 @@ def test_mixed_positive_negative():
     """
   
     mixed_array = [15, -3, 22, -1, 0, 6, -9, 8]
-    assert selectionSort(mixed_array) == sorted(mixed_array)
+    assert selection_sort(mixed_array) == sorted(mixed_array)
 
 def test_floats_and_integers():
     """
@@ -42,4 +47,4 @@ def test_floats_and_integers():
     """
   
     mixed_float_int_array = [4.5, 2, 9.3, 1, 7.8, 0, 3]
-    assert selectionSort(mixed_float_int_array) == sorted(mixed_float_int_array)
+    assert selection_sort(mixed_float_int_array) == sorted(mixed_float_int_array)
