80f311c47dcc075615f56ddc6eaffd7a929eae9b
Raj <81612989+Raj-mac24@users.noreply.github.com>
2024-09-10 14:03:32 -0400

    Create test_ms.py
    This Python file contains all the test cases which contain all the complex cases for an exhaustive run.

---
+++ test_ms.py
@@ -0,0 +1,45 @@
+import warnings
+import pytest
+from hw2_debugging import mergeSort
+
+warnings.filterwarnings('ignore')
+
+def test_large_array():
+    """
+    Test with a large array which contains 10 elements
+    """
+  
+    large_array = [100, 24, 56, 12, 98, 3, 67, 45, 29, 88]
+    assert mergeSort(large_array) == sorted(large_array)
+
+def test_array_with_duplicates():
+    """
+    Test with aa array which contains duplicate elements
+    """
+  
+    array_with_duplicates = [4, 2, 2, 8, 4, 6, 1, 9, 1]
+    assert mergeSort(array_with_duplicates) == sorted(array_with_duplicates)
+
+def test_negative_numbers():
+    """
+    Test with a large array which contains negative elements
+    """
+  
+    array_with_negatives = [-5, -1, -10, 3, 0, 7, -3]
+    assert mergeSort(array_with_negatives) == sorted(array_with_negatives)
+
+def test_mixed_positive_negative():
+    """
+    Test with a large array which contains mixed elements (positive & negative)
+    """
+  
+    mixed_array = [15, -3, 22, -1, 0, 6, -9, 8]
+    assert mergeSort(mixed_array) == sorted(mixed_array)
+
+def test_floats_and_integers():
+    """
+    Test with a large array which contains float & int type of elements
+    """
+  
+    mixed_float_int_array = [4.5, 2, 9.3, 1, 7.8, 0, 3]
+    assert mergeSort(mixed_float_int_array) == sorted(mixed_float_int_array)
